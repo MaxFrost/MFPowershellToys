d369e428495915fcd2452d9fbf71a2567d0f34be
James Maxson <MaximumFrost@gmail.com>
2017-04-07 15:51:56 -0500

    Updated to allow pipeline and array input

--- Install-RemoteMSI.ps1
+++ Install-RemoteMSI.ps1
@@ -1,32 +1,45 @@
 Param(
-    [Parameter(Mandatory=$true,Position=1)]
-    [string]$TargetComputer,
+    [cmdletbinding()]
+    [Parameter(
+        Mandatory=$true,
+        Position=0,
+        ValueFromPipeline=$true,
+        ValueFromPipelineByPropertyName=$true
+        )
+    ]
+    [Alias('Name')]
+    [string[]]$ComputerName,
+    [Parameter(
+        Mandatory=$true,
+        Position=1
+        )
+    ]
     [string]$InstallerPath
 )
-$ComputerName = $targetComputer
+
 $InstallFilePath = (get-childitem $InstallerPath).directoryName
 $InstallFile = (get-childitem $InstallerPath).name
-
-Write-Output "Installing: $InstallFilePath\$installFile to $ComputerName"
-if(test-wsman $ComputerName -ErrorAction Ignore){
-    Write-Verbose "Begin Copy Block"
-    Write-Verbose "Begin $InstallFile copy"
-    if (-not (test-path "\\$($ComputerName)\c$\Windows\Patches\" )){
-        new-item "\\$($ComputerName)\c$\Windows\Patches\" -ItemType Directory
+foreach ($computer in $computerName){
+    Write-Output "Installing: $InstallFilePath\$installFile to $Computer"
+    if(test-wsman $Computer -ErrorAction Ignore){
+        Write-Verbose "Begin Copy Block"
+        Write-Verbose "Begin $InstallFile copy"
+        if (-not (test-path "\\$($Computer)\c$\Windows\Patches\" )){
+            new-item "\\$($Computer)\c$\Windows\Patches\" -ItemType Directory
+        }
+        copy-item "$InstallFilePath\$InstallFile" "\\$($Computer)\c$\Windows\Patches\" -Force
+        Write-Verbose "$InstallFile copied"
+        Write-Verbose "End Copy Block"
+        Write-Verbose "Being Invoke-Command Block"
+        Write-Verbose "Target PC = $Computer"
+        invoke-command -computerName $Computer -ScriptBlock {
+            Start-Process C:\Windows\System32\msiexec.exe -ArgumentList /i, "C:\Windows\Patches\$($args[0])", /qn, /norestart -wait
+            get-eventlog -LogName application -newest 1 -InstanceId 1033 | select-object message
+        } -ArgumentList (,$InstallFile)
+        Write-Verbose "Invoke-Command Block completed"
+        Write-Output "Deployment Complete"
+        Remove-Item "\\$Computer\c$\Windows\Patches\$InstallFile"
+    }else{
+        write-warning -Message "unable to connect to WinRM @ $($computer)"
     }
-    copy-item "$InstallFilePath\$InstallFile" "\\$($ComputerName)\c$\Windows\Patches\" -Force
-    Write-Verbose "$InstallFile copied"
-    Write-Verbose "End Copy Block"
-    Write-Verbose "Being Invoke-Command Block"
-    Write-Verbose "Target PC = $ComputerName"
-    invoke-command -computerName $ComputerName -ScriptBlock {
-        Start-Process C:\Windows\System32\msiexec.exe -ArgumentList /i, "C:\Windows\Patches\$($args[0])", /qn, /norestart -wait
-        get-eventlog -LogName application -newest 1 -InstanceId 1033 | select-object message
-    } -ArgumentList (,$InstallFile)
-    Write-Verbose "Invoke-Command Block completed"
-    Write-Output "Deployment Complete"
-    Remove-Item "\\$ComputerName\c$\Windows\Patches\$InstallFile"
-}else{
-    write-warning -Message "unable to connect to WinRM"
-    return
 }
